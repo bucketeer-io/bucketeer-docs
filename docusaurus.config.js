2f263cd7759cca274cd00f54d4e2ed27af6544a1
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2022-09-28 19:50:13 +0900

    feat: add google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-WMMC2THNMZ',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
