b84c3d92449ed694fce6304fc55bea4d7354c13d
Heitor <heitor.j.tessaro@gmail.com>
2023-08-14 08:25:29 -0300

    style: update centeredImg component style

--- src/components/centered-img/CenteredImg.js
+++ src/components/centered-img/CenteredImg.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
-export default function Highlight({imgURL,wSize, alt, borderWidth}) {
+export default function Highlight({ imgURL, wSize, alt }) {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <img src={useBaseUrl(imgURL)} alt={alt} style={{ width: wSize, border: `${borderWidth} solid #555`, margin: '0 0 var(--ifm-paragraph-margin-bottom)' }}/>
+      <img
+        src={useBaseUrl(imgURL)}
+        alt={alt}
+        style={{
+          width: wSize,
+          // border: `${borderWidth} solid #555`,
+          margin: '0 0 var(--ifm-paragraph-margin-bottom)',
+          borderRadius: '14px', 
+          boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.15)', 
+        }}
+      />
     </div>
   );
 }
+
+
 
