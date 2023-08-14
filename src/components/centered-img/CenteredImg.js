27df1ea44a026d83bd80590b81f1861d256f9f1d
Heitor <heitor.j.tessaro@gmail.com>
2023-08-14 08:56:02 -0300

    docs: remove unecessary code centeredImg component

--- src/components/centered-img/CenteredImg.js
+++ src/components/centered-img/CenteredImg.js
@@ -9,7 +9,6 @@ export default function Highlight({ imgURL, wSize, alt }) {
         alt={alt}
         style={{
           width: wSize,
-          // border: `${borderWidth} solid #555`,
           margin: '0 0 var(--ifm-paragraph-margin-bottom)',
           borderRadius: '14px', 
           boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.15)', 
