44177dcf6d19acfc0f7a60780ed68dd850c54613
Heitor José Tessaro <heitor@writechoice.com.br>
2024-06-20 15:09:56 -0300

    chore: update docusaurus to 3.0.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -138,10 +139,7 @@ const config = {
         ],
       },
       prism: {
-        // theme: darkCodeTheme,
-        theme: lightCodeTheme,
-        // theme: require("prism-react-renderer/themes/vsDark"),
-        // theme: require("prism-react-renderer/themes/shadesOfPurple"),
+        theme: lightTheme,
         additionalLanguages: [
           'groovy',
           'kotlin',
