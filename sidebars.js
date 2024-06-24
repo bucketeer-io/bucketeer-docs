690f066f9f04739f3fa946e482977df4db519f2a
Gabriel Gonçalves <gabrielraeder@outlook.com.br>
2024-06-24 14:06:11 -0300

    openapi plugin test

--- sidebars.js
+++ sidebars.js
@@ -332,6 +332,20 @@ const sidebars = {
       label: 'Node JS',
       className: 'sidebar-nodejs',
     },
+  ],
+  openApiSidebar: [
+    {
+      type: "category",
+      label: "API",
+      link: {
+        type: "generated-index",
+        title: "API",
+        description: "test",
+        slug: "/category/api"
+      },
+      // @ts-ignore
+      items: require("./docs/api/sidebar.js")
+    }
   ]
 }
 
