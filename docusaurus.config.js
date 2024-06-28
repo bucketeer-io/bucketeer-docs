8c9c484f7e633191bb0732937e3137e736e0733c
Heitor José Tessaro <heitor@writechoice.com.br>
2024-06-28 08:15:34 -0300

    fix: fix themes bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer').themes.github
-const darkCodeTheme= require('prism-react-renderer').themes.dracula
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 const { webpackPlugin } = require('./plugins/webpack-plugin.cjs');
 
 
@@ -147,7 +148,7 @@ const config = {
       },
       prism: {
         // theme: darkCodeTheme,
-        theme: lightCodeTheme,
+        theme: lightTheme,
         // theme: require("prism-react-renderer/themes/vsDark"),
         // theme: require("prism-react-renderer/themes/shadesOfPurple"),
         additionalLanguages: [
